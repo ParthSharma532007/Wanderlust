fca1b14f17be05c4eeeb03b3c31ea33d92937cbd
Parth3673 <betu336699@gmail.com>
2026-01-13 22:48:38 +0530

    Change in Listing Schema

--- schema.js
+++ schema.js
@@ -1,15 +1,20 @@
-const Joi = require('joi');
+const Joi = require("joi");
 
 module.exports.listingSchema = Joi.object({
-    listing : Joi.object({
+    listing: Joi.object({
         title: Joi.string().required(),
         description: Joi.string().required(),
         location: Joi.string().required(),
         country: Joi.string().required(),
-        price: Joi.string().required().min(0),
-        image: Joi.string().allow("",null)
-    }).required(),
+
+        price: Joi.number().min(0).required(),
+
+        category: Joi.string().required(),
+
+        image: Joi.string().allow("", null)
+    }).required()
 });
+
 
 module.exports.reviewSchema = Joi.object({
     review : Joi.object({
